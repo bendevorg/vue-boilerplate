94605672cd1d1f936d0087cbed3c02dd59e5756c
GuiMac <guibasconti@gmail.com>
2018-07-27 08:21:07 -0300

    Add vue cookie

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import VueResource from 'vue-resource';
 import 'vuetify/dist/vuetify.min.css';
 import Vuex from 'vuex';
 import VueRouter from 'vue-router';
+import VueCookie from 'vue-cookie';
 import AsyncComputed from 'vue-async-computed';
 import Vuetify from 'vuetify';
 import App from './App.vue';
@@ -11,11 +12,11 @@ import { store } from './store/store';
 
 Vue.use(Vuetify);
 Vue.use(VueRouter);
+Vue.use(VueCookie);
 Vue.use(VueResource);
 Vue.use(Vuex);
 Vue.use(AsyncComputed);
 
-//Vue.http.options.root = process.env.BACKEND_HOST;
 const router = new VueRouter({
   routes,
   mode: 'history'
